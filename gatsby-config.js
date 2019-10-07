d4cb3e6de49179ef9820407930e828395c7333e1
aforian <alexian853@gmail.com>
2019-10-07 16:08:27 +0800

    add _redirects files.

--- gatsby-config.js
+++ gatsby-config.js
@@ -54,9 +54,10 @@ module.exports = {
           families: ['Avenir Next']
         }
       }
-    }
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-netlify`,
   ],
 }
